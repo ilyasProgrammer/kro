0fcf47cf92d386b8abfebf6d60ad0c9e4e63ccfc
IlyasRakhimkulov <i.rakhimkulov@arammeem.com>
2020-06-08 13:59:39 +0500

    More logging

--- kro_mods/models/task_automation.py
+++ kro_mods/models/task_automation.py
@@ -36,7 +36,7 @@ class TaskMod(models.Model):
                 msg_text = ''
                 date_diff = (now - date).days
                 log.info("Plan task date diff: %s %s", rec, str(date_diff))
-                log.info("Note 11 period: %s %s", rec, str(rec.get_note_period(now, 'PLAN 1 note')))
+                log.info("Note 1 period: %s %s", rec, str(rec.get_note_period(now, 'PLAN 1 note')))
                 if date_diff > 31 and 'PLAN 1 note' not in rec.notifications_history:
                     msg_text = u"Прошу вывести задание из планирования или, если задание не актуально, то его завершить."
                     subject = u"Планирование > 31 дня"
